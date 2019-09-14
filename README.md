2216bdcfd02240446df6107b136914178b03b580
daltonwalkerdw <53591219+daltonwalkerdw@users.noreply.github.com>
2019-09-14 14:02:10 -0400

    finished

--- README.md
+++ README.md
@@ -29,15 +29,22 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your team lead
 
 1. Describe the biggest difference between `.forEach` & `.map`.
+Map creates a new array where as forEach just executes a function for each array element.
 
 2. What is the difference between a function and a method?
+method is almost like a function but only used insides of classes, which take data from a class.
 
 3. What is closure?
+a concept that gives a nested function inside of a function access to outer scope.
 
 4. Describe the four rules of the 'this' keyword.
+ 1. Window Binding => Using "strict mode" to bypass window object of .this.
+ 2. Implicit binding => Makes use of .this keyword.
+ 3. New binding => Creates a new function based on a constructor, uses the "new" keyword.
+ 4. Explicit binding => Uses apply, call, and bind to point to .this.
 
 5. Why do we need super() in an extended class?
-
+So that the constructor has access to parents methods.
 ## Project Set up
 
 Follow these steps to set up and work on your project:
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,26 +6,37 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+let consume = (x, y, cb) => {
+   return cb(x, y)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add = (x, y) => {
+  return x + y;
+};
 
+let multiply = (x, y) => {
+ return x * y;
+};
 
+let greeting = (x, y) => {
+  return `Hello ${x} ${y}, nice to meet you!`
+};
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Because it is a nested function within a parent, it has access to anything the parent has.
 
 
 const external = "I'm outside the function";
